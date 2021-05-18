5cae0004720f8e8fc37cee252d7c75ce811f98b9
Rodimiro Cerrato E <rodyce@gmail.com>
2021-05-18 14:32:50 -0600

    Add XVG coin data

--- js/bip44-quick-gen.js
+++ js/bip44-quick-gen.js
@@ -1,54 +1,50 @@
-const bitcore = require('bitcore-lib');
-const Mnemonic = require('bitcore-mnemonic')
-const Message = require("bitcore-message");
-const readline = require("readline")
+const bitcore = require("bitcore-mnemonic/node_modules/bitcore-lib");
+const Mnemonic = require("bitcore-mnemonic");
+const readline = require("readline");
 
-const coin_data = require('./coin-data.js').coin_data
+const coin_data = require("./coin-data.js").coin_data;
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: true
-})
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
 
-rl.on('line', function(words) {
-    var code;
-    if (words.trim().length == 0) {
-        // Create random words
-        code = new Mnemonic(256);
-    } else {
-        code = new Mnemonic(words);
-    }
-    //var code = new Mnemonic(words);
-    console.log(code.toString())
+rl.on("line", function (words) {
+  var code;
+  if (words.trim().length == 0) {
+    // Create random words
+    code = new Mnemonic(256);
+  } else {
+    code = new Mnemonic(words);
+  }
+  //var code = new Mnemonic(words);
+  console.log(code.toString());
 
-    desired_coin_data = coin_data["efin"]
+  desired_coin_data = coin_data["xvg"];
 
-    bitcore.Networks.add(desired_coin_data["mainnet"].network_data);
+  bitcore.Networks.add(desired_coin_data["mainnet"].network_data);
 
-    var hdPrivateKey = code.toHDPrivateKey("", "efin/mainnet"); // empty passphrase
+  var hdPrivateKey = code.toHDPrivateKey("", "verge/mainnet"); // empty passphrase
 
-    console.log("BIP32 Root Key: " + hdPrivateKey.toString())
-    var derivationPath = hdPrivateKey
-        .derive(44, true) // 44'
-        .derive(coin_data["efin"]["mainnet"].bip44_id, true)
-        .derive(0, true)
-        .derive(0);
+  console.log("BIP32 Root Key: " + hdPrivateKey.toString());
+  var derivationPath = hdPrivateKey
+    .derive(44, true) // 44'
+    .derive(coin_data["xvg"]["mainnet"].bip44_id, true)
+    .derive(0, true)
+    .derive(0);
 
-    for (var i = 0; i < 10; i++) {
-        var myPath = derivationPath.derive(i)
-        var privKey = myPath.privateKey;
-        var pubKey = privKey.toPublicKey();
-        var address = privKey.toAddress();
+  for (var i = 0; i < 1000; i++) {
+    var myPath = derivationPath.derive(i);
+    var privKey = myPath.privateKey;
+    var pubKey = privKey.toPublicKey();
+    var address = privKey.toAddress();
 
-        console.log(`${i} - ${address.toString()}`);
-        /*
-        console.log("    " + pubKey.toString() + ", " + privKey.toString());
-        console.log(privKey.toWIF());
-        */
-        //var script = bitcore.Script.buildPublicKeyHashOut(address);
-        
-        //console.log("SCRIPT: " + script.toHex());
-    }
-})
+    console.log(`${i} - ${address.toString()}`);
+    console.log("    " + pubKey.toString() + ", " + privKey.toString());
+    console.log(privKey.toWIF());
+    //var script = bitcore.Script.buildPublicKeyHashOut(address);
 
+    //console.log("SCRIPT: " + script.toHex());
+  }
+});
--- js/coin-data.js
+++ js/coin-data.js
@@ -1,46 +1,61 @@
 module.exports = {
-    coin_data: {
-        "efin": {
-            "mainnet": {
-                network_data: {
-                    name: "efin/mainnet",
-                    alias: "efin livenet",
-        
-                    pubkeyhash: 0x21,
-                    privatekey: 0x5c,
-                    scripthash: 0x3c,
-                    xpubkey: 0x011c3bed,
-                    xprivkey: 0x011c3488
-                },
-                bip44_id: 3714 // 3714'
-            },
-            "testnet": {
-                network_data: {
-                    name: "efin/testnet",
-                    alias: "efin testnet",
-        
-                    pubkeyhash: 0x5c,
-                    privatekey: 0xfe,
-                    scripthash: 0x7a,
-                    xpubkey: 0x04324de3,
-                    xprivkey: 0x0432467f
-                },
-                bip44_id: 3715 // 3715'
-            },
+  coin_data: {
+    efin: {
+      mainnet: {
+        network_data: {
+          name: "efin/mainnet",
+          alias: "efin livenet",
+
+          pubkeyhash: 0x21,
+          privatekey: 0x5c,
+          scripthash: 0x3c,
+          xpubkey: 0x011c3bed,
+          xprivkey: 0x011c3488,
         },
-        "tpay": {
-            "mainnet": {
-                network_data: {
-                    name: 'tpay/mainnet',
-                    alias: 'tpay livenet',
-                    pubkeyhash: 0x41,
-                    privatekey: 0xb3,
-                    scripthash: 0x7e,
-                    xpubkey: 0x0488B21E,
-                    xprivkey: 0x0488ADE4
-                },
-                bip44_id: 265
-            }
-        }
-    }
-}
+        bip44_id: 3714, // 3714'
+      },
+      testnet: {
+        network_data: {
+          name: "efin/testnet",
+          alias: "efin testnet",
+
+          pubkeyhash: 0x5c,
+          privatekey: 0xfe,
+          scripthash: 0x7a,
+          xpubkey: 0x04324de3,
+          xprivkey: 0x0432467f,
+        },
+        bip44_id: 3715, // 3715'
+      },
+    },
+    tpay: {
+      mainnet: {
+        network_data: {
+          name: "tpay/mainnet",
+          alias: "tpay livenet",
+          pubkeyhash: 0x41,
+          privatekey: 0xb3,
+          scripthash: 0x7e,
+          xpubkey: 0x0488b21e,
+          xprivkey: 0x0488ade4,
+        },
+        bip44_id: 265,
+      },
+    },
+    xvg: {
+      mainnet: {
+        network_data: {
+          name: "verge/mainnet",
+          alias: "verge livenet",
+          pubkeyhash: 0x1e,
+          privatekey: 0xb3,
+          scripthash: 0x21,
+          xpubkey: 0x0488b21e,
+          xprivkey: 0x0488ade4,
+          wif: 0x9e,
+        },
+        bip44_id: 77,
+      },
+    },
+  },
+};
